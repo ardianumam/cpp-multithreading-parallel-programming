02ab1c471e72beb0658da740836dca09fd23203a
Ardian Umam <ardianumam@gmail.com>
2025-02-01 11:08:05 +0800

    multiple read and few write

---
+++ 02_working_with_shared_data/08_multiple_read_few_write.cc
@@ -0,0 +1,48 @@
+// Mutex example
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <vector>
+
+std::mutex mut;
+
+// Shared variable
+int x = 0;
+
+void write()
+{
+	std::lock_guard<std::mutex> lck_guard(mut);
+
+	// Start of critical section
+	++x;
+	// End of critical section
+}
+
+void read()
+{
+	std::lock_guard<std::mutex> lck_guard(mut); // still don't know why do we need to put mutex for read???
+
+	// Start of critical section
+	using namespace std::literals;
+    std::cout << x << std::endl;
+	std::this_thread::sleep_for(100ms);
+	// End of critical section
+}
+
+int main()
+{
+	std::vector<std::thread> threads;
+
+	for (int i = 0; i < 20; ++i)
+		threads.push_back(std::thread(read));
+
+	threads.push_back(std::thread(write));
+	threads.push_back(std::thread(write));
+
+	for (int i = 0; i < 20; ++i)
+		threads.push_back(std::thread(read));
+
+	for (auto& thr : threads)
+		thr.join();
+}
