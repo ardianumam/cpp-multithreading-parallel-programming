1a88edb19c02204e55e3fb3b14bc84080191c8b4
Ardian Umam <ardianumam@gmail.com>
2025-02-02 17:38:31 +0800

    asynchronous programming with std::async

---
+++ 04_asynchronous_programming/03_async_with_deferred_policy.cc
@@ -0,0 +1,30 @@
+#include <future>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+
+using namespace std::literals;
+
+int task()
+{
+    std::cout << "Executing task() in thread with ID: ";
+    std::cout << std::this_thread::get_id() << std::endl;
+    std::this_thread::sleep_for(5s);
+    std::cout << "Returning from task()\n";
+    return 42;
+}
+
+int main()
+{
+    std::cout << "In main thread with ID: " << std::this_thread::get_id() << '\n';
+
+    // Async option
+    std::cout << "Calling async with option \"deferred\"\n";
+    std::future<int> result_async = std::async(std::launch::deferred, task);
+    std::cout << "After the async call\n";
+    std::cout << "Async get() result: " << result_async.get() << '\n';
+    std::cout << "After the async get()\n";
+
+    return 0;
+}
